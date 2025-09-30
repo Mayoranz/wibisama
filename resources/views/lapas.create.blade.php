071c846b6ab3a26ea6b83380abb3d94c161e9742
= <=>
2025-09-30 20:01:45 +0700

    backend

--- resources/views/lapas.create.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>Tambah Lapas</h1>
-<form action="{{ route('lapas.store') }}" method="POST">
-    @csrf
-    Nama: <input type="text" name="nama"><br>
-    Lokasi: <input type="text" name="lokasi"><br>
-    Kapasitas: <input type="text" name="kapasitas"><br>
-    <button type="submit">Simpan</button>
-</form>
--- resources/views/lapas.edit.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>Edit Lapas</h1>
-<form action="{{ route('lapas.update', $lapas->id) }}" method="POST">
-    @csrf
-    @method('PUT')
-    Nama: <input type="text" name="nama" value="{{ $lapas->nama }}"><br>
-    Lokasi: <input type="text" name="lokasi" value="{{ $lapas->lokasi }}"><br>
-    Kapasitas: <input type="text" name="kapasitas" value="{{ $lapas->kapasitas }}"><br>
-    <button type="submit">Update</button>
-</form>
