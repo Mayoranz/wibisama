2882d7588b976807b2bea40af5ba9319a82d837d
= <=>
2025-10-01 08:30:22 +0700

    db baru

--- database/migrations/2025_09_30_075339_create_kriminal_table.php
+++ database/migrations/2025_09_30_075339_create_kriminal_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('kriminal', function (Blueprint $table) {
+        Schema::create('lapas', function (Blueprint $table) {
           $table->id();
             $table->string('nama',50);
             $table->varchar('lokasi',500);
